96636ce7dbedbd84edd2ba2701b6c4018d47db04
Drilon Syla <58865482+drilaexe@users.noreply.github.com>
2024-01-23 07:39:31 +0100

    Update Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -88,6 +88,12 @@ const submit = () => {
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
+<Link
+                    :href="route('register')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Dont have an account? Register
+                </Link>
             </div>
         </form>
     </GuestLayout>
